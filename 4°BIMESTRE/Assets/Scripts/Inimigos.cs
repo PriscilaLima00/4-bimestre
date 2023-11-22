11d8e9e6ce28f84da45195c882ea55197dfa991c
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-11-22 09:28:24 -0300

    Lasers dos Inimigos

--- 4°BIMESTRE/Assets/Scripts/Inimigos.cs
+++ 4°BIMESTRE/Assets/Scripts/Inimigos.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Inimigos : MonoBehaviour
-{  
+{
+    public GameObject laserDoInimigo;
+    public Transform localDoDisparo;
+    
     public float velocidadeDoInimigo;
+
+    public float tempoMaximoEntreOsLesers;
+    public float tempoAtualDosLasers;
+
+    public bool inimigoAtirador;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +24,25 @@ public class Inimigos : MonoBehaviour
     void Update()
     {
         MovimentarInimigo();
+        if (inimigoAtirador == true)
+        {
+            AtirarLaser();
+        }
     }
     
     private void MovimentarInimigo()
     {
         transform.Translate(Vector3.down * velocidadeDoInimigo * Time.deltaTime);
+    }
+
+    private void AtirarLaser()
+    {
+        tempoAtualDosLasers -= Time.deltaTime;
+        
+        if (tempoAtualDosLasers <= 0)
+        {
+            Instantiate(laserDoInimigo, localDoDisparo.position, Quaternion.Euler(0f, 0f, 90f));
+            tempoAtualDosLasers = tempoMaximoEntreOsLesers;
+        }
     }
 }
