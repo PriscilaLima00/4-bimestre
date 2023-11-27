c28bf529c7528bbeddf9b37744f1735417992d42
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-11-27 08:33:02 -0300

    Mostrando a pontuacao na tela

--- 4°BIMESTRE/Assets/Scripts/GameManager.cs
+++ 4°BIMESTRE/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager intance;
+
+    public Text textoDePontuacaoAtual;
     
     public int pontuacaoAtual;
 
@@ -17,6 +20,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         pontuacaoAtual = 0;
+        textoDePontuacaoAtual.text = "PONTUAÇÃO: " + pontuacaoAtual;
     }
 
     // Update is called once per frame
@@ -28,5 +32,6 @@ public class GameManager : MonoBehaviour
     public void AumentarPontuacao(int pontosParaGanhar)
     {
         pontuacaoAtual += pontosParaGanhar;
+        textoDePontuacaoAtual.text = "PONTUAÇÃO: " + pontuacaoAtual;
     }
 }
