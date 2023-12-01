dd77f0591d3260f182f22c3e8b9e0cd8d65e7df4
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2023-12-01 09:30:55 -0300

    Power-Up criados

---
+++ 4°BIMESTRE/Assets/Scripts/ItensColetáveis.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItensColetáveis : MonoBehaviour
+{
+    public bool itemDeEscudo;
+
+    public bool itemDeLaserDuplo;
+
+    public bool itemDeVida;
+    public int vidaParaDar;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (itemDeEscudo == true)
+            {
+                other.gameObject.GetComponent<VidaDoJogador>().AtivarEscudo();
+            }
+
+            if (itemDeLaserDuplo == true)
+            {
+                other.gameObject.GetComponent<ControleDoJogador>().temLaserDuplo = false;
+
+                other.gameObject.GetComponent<ControleDoJogador>().tempoAtualDosLasersDuplos = other.gameObject.GetComponent<ControleDoJogador>().tempoMaximoDosLasersDuplos;
+                
+                other.gameObject.GetComponent<ControleDoJogador>().temLaserDuplo = true;
+            }
+
+            if (itemDeVida == true)
+            {
+                other.gameObject.GetComponent<VidaDoJogador>().GanharVida(vidaParaDar);
+            }
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
