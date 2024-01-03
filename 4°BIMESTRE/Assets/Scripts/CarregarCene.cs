55f40459fd12818cd9e5017eb7ba59fc11d2134a
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-01-03 08:30:10 -0300

    ainda tem bugues

--- 4°BIMESTRE/Assets/Scripts/CarregarCene.cs
+++ 4°BIMESTRE/Assets/Scripts/CarregarCene.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarregarCene : MonoBehaviour
 {
+
+    public string cenaParaCarregar;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(cenaParaCarregar);
     }
 
     // Update is called once per frame
